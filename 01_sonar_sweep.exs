308ab76f0d0cdef9bbe51e21bc0fd4719af8fabe
Nikolay Vasilenko <nick.v@voiso.com>
2021-12-02 11:23:10 +0300

    Day 1: Sonar Sweep (part two)

--- 01_sonar_sweep.exs
+++ 01_sonar_sweep.exs
@@ -6,5 +6,20 @@ defmodule Sonar do
   def count([curr | rest], _, result), do: count(rest, curr, result)
 end
 
+defmodule AdvancedSonar do
+  def count(depths) do
+    depths
+    |> Stream.chunk_every(3, 1, :discard)
+    |> Stream.map(fn chunk -> Enum.sum(chunk) end)
+    |> Enum.reduce({nil, 0}, fn
+      curr, {prev, result} when curr > prev -> {curr, result + 1}
+      curr, {_, result} -> {curr, result}
+    end)
+    |> elem(1)
+  end
+end
+
 depths = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
-IO.puts("Result: #{Sonar.count(depths)}")
+
+IO.puts("Result (part one): #{Sonar.count(depths)}")
+IO.puts("Result (part two): #{AdvancedSonar.count(depths)}")
