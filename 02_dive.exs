29c26b55bf8a0d8422234a9c19329e6835cb89f4
Nikolay Vasilenko <nick.v@voiso.com>
2021-12-02 11:46:57 +0300

    Day 2: Dive! (part two)

--- 02_dive.exs
+++ 02_dive.exs
@@ -14,6 +14,28 @@ defmodule Submarine do
   end
 end
 
+defmodule AdvancedSubmarine do
+  def position([]), do: 0
+  def position(commands), do: position(commands, 0, 0, 0)
+  def position([], _, depth, hor), do: depth * hor
+
+  def position(["up " <> pos | rest], aim, depth, hor),
+    do: position(rest, aim - parse(pos), depth, hor)
+
+  def position(["down " <> pos | rest], aim, depth, hor),
+    do: position(rest, aim + parse(pos), depth, hor)
+
+  def position(["forward " <> pos | rest], aim, depth, hor) do
+    value = parse(pos)
+    position(rest, aim, depth + aim * value, hor + value)
+  end
+
+  defp parse(str) do
+    {int, _} = Integer.parse(str)
+    int
+  end
+end
+
 commands = [
   "forward 5",
   "down 5",
@@ -23,4 +45,5 @@ commands = [
   "forward 2"
 ]
 
-IO.puts("Result: #{Submarine.position(commands)}")
+IO.puts("Result (part one): #{Submarine.position(commands)}")
+IO.puts("Result (part two): #{AdvancedSubmarine.position(commands)}")
