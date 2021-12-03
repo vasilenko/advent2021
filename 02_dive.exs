8f7e871fb41cf992503a286458ca34436116a33f
Nikolay Vasilenko <nick.v@voiso.com>
2021-12-03 10:45:17 +0300

    Refactor Day 2

--- 02_dive.exs
+++ 02_dive.exs
@@ -1,38 +1,36 @@
 defmodule Submarine do
+  defdelegate to_integer(string), to: String
+
   def position([]), do: 0
   def position(commands), do: position(commands, 0, 0)
   def position([], depth, hor), do: depth * hor
-  def position(["up " <> pos | rest], depth, hor), do: position(rest, depth - parse(pos), hor)
-  def position(["down " <> pos | rest], depth, hor), do: position(rest, depth + parse(pos), hor)
+
+  def position(["up " <> pos | rest], depth, hor),
+    do: position(rest, depth - to_integer(pos), hor)
+
+  def position(["down " <> pos | rest], depth, hor),
+    do: position(rest, depth + to_integer(pos), hor)
 
   def position(["forward " <> pos | rest], depth, hor),
-    do: position(rest, depth, hor + parse(pos))
-
-  defp parse(str) do
-    {int, _} = Integer.parse(str)
-    int
-  end
+    do: position(rest, depth, hor + to_integer(pos))
 end
 
 defmodule AdvancedSubmarine do
+  defdelegate to_integer(string), to: String
+
   def position([]), do: 0
   def position(commands), do: position(commands, 0, 0, 0)
   def position([], _, depth, hor), do: depth * hor
 
   def position(["up " <> pos | rest], aim, depth, hor),
-    do: position(rest, aim - parse(pos), depth, hor)
+    do: position(rest, aim - to_integer(pos), depth, hor)
 
   def position(["down " <> pos | rest], aim, depth, hor),
-    do: position(rest, aim + parse(pos), depth, hor)
+    do: position(rest, aim + to_integer(pos), depth, hor)
 
   def position(["forward " <> pos | rest], aim, depth, hor) do
-    value = parse(pos)
+    value = to_integer(pos)
     position(rest, aim, depth + aim * value, hor + value)
-  end
-
-  defp parse(str) do
-    {int, _} = Integer.parse(str)
-    int
   end
 end
 
