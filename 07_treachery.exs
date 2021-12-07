24f960e32092459ce0406270a90f9bb49d7edd8e
Nikolay Vasilenko <nick.v@voiso.com>
2021-12-07 09:29:53 +0300

    Refactor Day 7

--- 07_treachery.exs
+++ 07_treachery.exs
@@ -1,16 +1,12 @@
 defmodule Fuel do
-  def count1(data), do: count(data, &abs(&1 - &2))
-  def count2(data), do: count(data, &Enum.sum(0..abs(&1 - &2)))
+  def count1(data), do: count(data, &(&1))
+  def count2(data), do: count(data, &((1 + &1) / 2 * &1))
 
   defp count(data, fun) do
     {min, max} = Enum.min_max(data)
 
     min..max
-    |> Enum.map(fn pos ->
-      data
-      |> Enum.map(&fun.(pos, &1))
-      |> Enum.sum()
-    end)
+    |> Enum.map(fn pos -> Enum.reduce(data, 0, &fun.(abs(pos - &1)) + &2) end)
     |> Enum.min()
   end
 end
